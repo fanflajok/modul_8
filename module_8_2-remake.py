e12cc2020716c3e6424ad5da04d25cfab2d2573a
Victor <fanflajok@gmail.com>
2024-09-22 13:22:13 +0300

    Add files via upload
    Работа над ошибками module_8_2

---
+++ module_8_2-remake.py
@@ -0,0 +1,29 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы {i}')
+    kor = (result, incorrect_data)
+    return kor
+def calculate_average(numbers):
+        try:
+            summa = personal_sum(numbers)
+            avg = summa[0] /(len(numbers) - summa[1])
+            return avg
+        except ZeroDivisionError:
+            avg = 0
+            return avg
+        except TypeError:
+            print('В numbers записан некорректный тип данных')
+        return None
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
+
